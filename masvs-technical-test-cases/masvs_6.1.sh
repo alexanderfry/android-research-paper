9081f738b6a6ae0eaedcba5b39e9cd3711a6d707
Alexander Fry <alex@strongcrypto.com>
2019-11-30 13:31:20 -0500

    Technical Test Case for MASVS 6.1

---
+++ masvs-technical-test-cases/masvs_6.1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# MASVS 6.1
+# Run the following command from the Drozer Console
+# run app.package.info -a com.android.insecurebankv2 > output.txt
+# Exit Drozer Console by typing exit
+# exit
+# Identify dangerous permissions
+# cat output.txt | egrep -i 'calendar|camera|contacts|location|microphone|phone|sensors|sms|storage' 
